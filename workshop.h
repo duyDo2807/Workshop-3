9ad42b94cd657d91345e86bc5b00fc0ee1c0baad
duyDo2807 <a1881078@adelaide.edu.au>
2023-08-07 23:00:59 +0930

    Workshop 3

--- workshop.h
+++ workshop.h
@@ -1,12 +1,28 @@
-#ifndef WORKSHOP_H
-#define WORKSHOP_H
+#include <iostream>
 
-void changeValue(double* ptr);
+void changeValue(double* ptr) { *ptr = 42; }
 
-void printArray(double* ptr, int length);
+void printArray(double* ptr, int length) {
+  for (int i = 0; i < length; i++) {
+    std::cout << ptr[i] << " ";
+  }
+  std::cout << std::endl;
+}
 
-double arrayMax(double* ptr, int length);
+double arrayMax(double* ptr, int length) {
+  double max = ptr[0];
+  for (int i = 1; i < length; i++) {
+    if (max < ptr[i]) {
+      max = ptr[i];
+    }
+  }
+  return max;
+}
 
-double* dynamicArray(int N, double M);
-
-#endif
+double* dynamicArray(int N, double M) {
+  double* newArray = new double[N];
+  for (int i = 0; i < N; i++) {
+    newArray[i] = M;
+  }
+  return newArray;
+}
